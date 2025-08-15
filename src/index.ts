ad459fd07e7462a6760881a3c530fdc2b88d2050
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2025-08-15 14:28:28 +0900

    GitHub GraphQL APIにアクセスするコネクタを試作

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,182 @@
-/**
- * Copyright 2023 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { hello } from './example-module';
+import Fields = GoogleAppsScript.Data_Studio.Fields;
 
-console.log(hello());
+const cc = DataStudioApp.createCommunityConnector();
+const API_KEY_PROPERTY = 'githubConnector.key';
+
+/* 認証 */
+
+function getAuthType(): GetAuthTypeResponse {
+  return (
+    cc
+      .newAuthTypeResponse()
+      .setAuthType(cc.AuthType.KEY)
+      // NOTE: コネクタ認証時のヘルプページを案内する場合に
+      // .setHelpUrl('https://www.example.org/connector-auth-help')
+      .build()
+  );
+}
+
+function resetAuth() {
+  const userProperties = PropertiesService.getUserProperties();
+  userProperties.deleteProperty(API_KEY_PROPERTY);
+}
+
+function isAuthValid() {
+  const userProperties = PropertiesService.getUserProperties();
+  const apiKey = userProperties.getProperty(API_KEY_PROPERTY);
+  return apiKey !== null && apiKey !== '';
+}
+
+function setCredentials(request: KeyCredentials): SetCredentialsResponse {
+  const key = request.key;
+
+  // NOTE: APIキーの有効性を保存前に検証する場合に
+  // const validKey = checkForValidKey(key);
+  // if (!validKey) {
+  //   return {
+  //     errorCode: 'INVALID_CREDENTIALS'
+  //   };
+  // }
+
+  const userProperties = PropertiesService.getUserProperties();
+  userProperties.setProperty(API_KEY_PROPERTY, key);
+  return {
+    errorCode: 'NONE',
+  };
+}
+
+/* 設定 */
+
+function getConfig() {
+  const config = cc.getConfig();
+  config
+    .newInfo()
+    .setId('instructions')
+    .setText('データ取得対象を指定してください🐬');
+  config
+    .newTextInput()
+    .setId('repoSearchQuery')
+    .setName('GitHubのリポジトリ検索クエリ')
+    .setHelpText(
+      'https://docs.github.com/ja/search-github/getting-started-with-searching-on-github/understanding-the-search-syntax'
+    )
+    .setPlaceholder('owner:lagenorhynque sort:updated');
+
+  // NOTE: データ取得時に期間指定のパラメータが必要な場合に
+  // config.setDateRangeRequired(true);
+
+  return config.build();
+}
+
+/* スキーマ */
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function getSchema(request: GetSchemaRequest): GetSchemaResponse {
+  const fields = getFields().build();
+  return { schema: fields };
+}
+
+function getFields() {
+  const fields = cc.getFields();
+  const types = cc.FieldType;
+
+  fields.newDimension().setId('name').setType(types.TEXT);
+  fields.newMetric().setId('description').setType(types.TEXT);
+  fields.newMetric().setId('url').setType(types.URL);
+  fields.newMetric().setId('createdAt').setType(types.TEXT);
+  fields.newMetric().setId('updatedAt').setType(types.TEXT);
+  fields.newMetric().setId('stargazerCount').setType(types.NUMBER);
+
+  return fields;
+}
+
+/* データ */
+
+function getData(request: GetDataRequest): GetDataResponse {
+  const requestedFieldIds = request.fields.map(field => field.name);
+  const requestedFields = getFields().forIds(requestedFieldIds);
+  const userProperties = PropertiesService.getUserProperties();
+  const apiKey = userProperties.getProperty(API_KEY_PROPERTY);
+  const parsedResponseData = callGitHubGraphQLApi(
+    apiKey ?? '',
+    request.configParams?.repoSearchQuery ?? ''
+  );
+  return {
+    schema: requestedFields.build(),
+    rows: responseToRows(requestedFields, parsedResponseData),
+  };
+}
+
+function responseToRows(
+  requestedFields: Fields,
+  responseData: QueryResponseData
+): GetDataRows {
+  const fieldIds = requestedFields.asArray().map(f => f.getId());
+  return responseData.search.nodes.map(node => {
+    return {
+      values: fieldIds.map(fieldId => {
+        switch (fieldId) {
+          case 'name':
+            return node.name;
+          case 'description':
+            return node.description;
+          case 'url':
+            return node.url;
+          case 'createdAt':
+            return node.createdAt;
+          case 'updatedAt':
+            return node.updatedAt;
+          case 'stargazerCount':
+            return node.stargazerCount;
+          default:
+            return '';
+        }
+      }),
+    };
+  });
+}
+
+const QUERY = `
+query ($repoSearchQuery: String!, $first: Int = 100) {
+  search(type: REPOSITORY, query: $repoSearchQuery, first: $first) {
+    nodes {
+      ... on Repository {
+        name
+        description
+        url
+        createdAt
+        updatedAt
+        stargazerCount
+      }
+    }
+  }
+}
+`;
+
+interface QueryResponseData {
+  search: {
+    nodes: {
+      name: string;
+      description: string;
+      url: string;
+      createdAt: string;
+      updatedAt: string;
+      stargazerCount: number;
+    }[];
+  };
+}
+
+function callGitHubGraphQLApi(apiKey: string, repoSearchQuery: string) {
+  const url = 'https://api.github.com/graphql';
+  const body = {
+    query: QUERY,
+    variables: { repoSearchQuery: repoSearchQuery },
+  };
+  const response = UrlFetchApp.fetch(url, {
+    method: 'post',
+    contentType: 'application/json',
+    headers: { Authorization: `bearer ${apiKey}` },
+    payload: JSON.stringify(body),
+  });
+  return JSON.parse(response.getContentText()).data;
+}
